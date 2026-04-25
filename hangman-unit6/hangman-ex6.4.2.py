b5eb88cdcfd3952ddc3b3e2e967541b04033ef06
Noga <noga.a@harmonizseed.com>
2026-04-25 13:33:06 +0300

    בתרגיל הזה תפעלו בהתאמה לתו החדש שהשחקן ניחש: או שתוסיפו אותו לרשימת הניחושים, או שתדפיסו הודעה אם אי אפשר להוסיפו.
    כתבו פונקציה שנקראת try_update_letter_guessed המוגדרת כך:
    
    def try_update_letter_guessed(letter_guessed, old_letters_guessed):
    ערכי הקבלה של הפונקציה
    מחרוזת שנקראת letter_guessed. המחרוזת מייצגת את התו שנקלט מהשחקן.
    רשימה שנקראת old_letters_guessed. הרשימה מכילה את האותיות שהשחקן ניחש עד כה.
    פעולת הפונקציה
    אם התו תקין (כלומר אות אנגלית אחת) ולא ניחשו אותו בעבר, הפונקציה תוסיף את התו letter_guessed לרשימה old_letters_guessed. לאחר מכן תחזיר ערך אמת (True) שמציין שההוספה התבצעה בהצלחה.
    אם התו אינו תקין (כלומר אינו אות אנגלית אחת) או שהוא נמצא כבר ברשימת הניחושים, הפונקציה תדפיס את התו X (האות הגדולה X) ומתחתיו את הרשימה old_letters_guessed כמחרוזת של אותיות קטנות שממוינות מהקטן לגדול ומופרדות זו מזו בחצים (חץ מורכב מהסימנים: <-, ראו פלט לדוגמה). הדפסת האיברים נועדה להזכיר לשחקן אילו תווים הוא ניחש כבר. בסיום, הפונקציה תחזיר ערך שקר (False) שמשמעותו שאין אפשרות להוסיף את התו לרשימת התווים שניחשו כבר.

---
+++ hangman-unit6/hangman-ex6.4.2.py
@@ -0,0 +1,20 @@
+def check_valid_input(letter_guessed, old_letters_guessed):
+    if len(letter_guessed) > 1:
+        return False
+    if letter_guessed.isalpha()== False:
+        return False
+    if letter_guessed.lower() in old_letters_guessed:
+        return False
+    return True
+
+
+def try_update_letter_guessed(letter_guessed, old_letters_guessed):
+
+    if len(letter_guessed) ==1 and letter_guessed.isalpha() and letter_guessed.lower() not in old_letters_guessed:
+        old_letters_guessed.append(letter_guessed.lower())
+        return True
+    else:
+        print("X")
+        old_letters_guessed.sort()
+        print(' -> '.join(old_letters_guessed))
+        return False
